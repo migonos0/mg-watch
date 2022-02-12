1f7a4e2d15fab67642bd6764cd05a1c6430a0abb
Miguel Rodríguez <miguelrodrii@outlook.com>
2022-02-11 22:50:58 -0500

    init

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Inicio</title>
+</svelte:head>
+<h1 class="text-3xl font-bold underline bg-slate-500">Hello world!</h1>
