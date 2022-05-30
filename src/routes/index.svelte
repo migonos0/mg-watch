04d8df9e995ae373ccf1f53a740678217d13f5eb
migonos0 <34117518+migonos0@users.noreply.github.com>
2022-05-30 22:53:31 +0000

    modified index file

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
 <svelte:head>
     <title>Inicio</title>
 </svelte:head>
-<h1 class="text-3xl font-bold underline bg-slate-500">Hello world!</h1>
+<h1 class="text-3xl font-bold underline bg-slate-500">Hello World!</h1>
