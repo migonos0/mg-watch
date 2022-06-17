4e1a77164ab7d223aaccf0341559ed35b06e3ce4
migonos0 <34117518+migonos0@users.noreply.github.com>
2022-06-17 17:42:55 +0000

    added fonts and default styles for all the website

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,27 @@
     import '../app.css';
 </script>
 
+<svelte:head
+    ><link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin="anonymous"
+    />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Anek+Malayalam&family=Kdam+Thmor+Pro&display=swap"
+        rel="stylesheet"
+    /></svelte:head
+>
+
 <slot />
+
+<footer class="bg-gray-700 absolute bottom-0 w-full text-center text-white">
+    <p class="text-xs">Buit with love by</p>
+    <strong
+        ><a
+            href="https://www.github.com/migonos0"
+            target="https://www.github.com/migonos0">@migonos0</a
+        ></strong
+    >
+</footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,3 @@
 <svelte:head>
     <title>Inicio</title>
 </svelte:head>
-<h1 class="text-3xl font-bold underline bg-slate-500">Hello World!</h1>
