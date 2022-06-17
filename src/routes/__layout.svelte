c18cf984fd08ef3d235c978dd06aee32e3e7bc82
Miguel Rodríguez <miguelrodrii@outlook.com>
2022-06-17 18:38:02 -0500

    Implemented dark theme and main layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,42 +2,67 @@
     import '../app.css';
     import {classList} from 'svelte-body';
     import {onMount} from 'svelte';
-    import {writable} from 'svelte/store';
+    import {load} from '../utilities/localStorage.utility';
 
-    let isDarkModeEnabledValue;
+    let isDarkThemeEnabled: boolean;
 
-    export const isDarkModeEnabled = writable(true);
-    isDarkModeEnabled.subscribe((value) => {
-        window.localStorage.setItem('global_theme', value ? 'dark' : 'light');
-        isDarkModeEnabledValue = value;
+    onMount(() => {
+        isDarkThemeEnabled =
+            load('global_theme') === null
+                ? window.matchMedia('(prefers-color-scheme: dark)').matches
+                : load('global_theme') !== 'light';
     });
+
+    function toggleTheme() {
+        isDarkThemeEnabled = !isDarkThemeEnabled;
+
+        localStorage.setItem(
+            'global_theme',
+            isDarkThemeEnabled ? 'dark' : 'light'
+        );
+    }
 </script>
 
 <svelte:body
-    use:classList={isDarkModeEnabledValue ? 'dark bg-gray-900' : 'bg-white'} />
+    use:classList={isDarkThemeEnabled ? 'dark bg-gray-900 text-white' : 'bg-white'} />
 
 <slot />
 
 <button
-    class="fixed bottom-4 right-4 z-10 dark:bg-stone-900 dark:text-white p-3 rounded-full"
-    ><svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        stroke-width="2"
-    >
-        <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-        />
-    </svg></button
+    on:click={toggleTheme}
+    class="fixed bottom-4 right-2 z-10 dark:bg-stone-900 dark:text-white text-stone-900 bg-white p-3 shadow-lg border-2 border-stone-900 rounded-full"
 >
+    {#if isDarkThemeEnabled}<svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+        </svg>{:else}<svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
+        </svg>{/if}
+</button>
 
 <footer
-    class="dark:bg-gray-700 absolute bottom-0 w-full text-center text-white"
+    class="bg-gray-700 absolute bottom-0 w-full text-center text-white"
 >
     <p class="text-xs">Buit with love by</p>
     <strong
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,12 @@
 <svelte:head>
-    <title>Inicio</title>
+    <title>Index</title>
 </svelte:head>
+
+<h1 class="text-xl">
+    Welcome Aboard
+</h1>
+<p>
+    My name is Miguel Rodríguez, I do programming and other stuff :b
+</p>
+
+<div></div>
