2720bf5de03f4fafa65cdd96a8c4162d2c5144d9
migonos0 <34117518+migonos0@users.noreply.github.com>
2022-06-17 21:16:08 +0000

    failing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,23 +1,44 @@
-<script>
+<script lang="ts">
     import '../app.css';
+    import {classList} from 'svelte-body';
+    import {onMount} from 'svelte';
+    import {writable} from 'svelte/store';
+
+    let isDarkModeEnabledValue;
+
+    export const isDarkModeEnabled = writable(true);
+    isDarkModeEnabled.subscribe((value) => {
+        window.localStorage.setItem('global_theme', value ? 'dark' : 'light');
+        isDarkModeEnabledValue = value;
+    });
 </script>
 
-<svelte:head
-    ><link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossorigin="anonymous"
-    />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Anek+Malayalam&family=Kdam+Thmor+Pro&display=swap"
-        rel="stylesheet"
-    /></svelte:head
->
+<svelte:body
+    use:classList={isDarkModeEnabledValue ? 'dark bg-gray-900' : 'bg-white'} />
 
 <slot />
 
-<footer class="bg-gray-700 absolute bottom-0 w-full text-center text-white">
+<button
+    class="fixed bottom-4 right-4 z-10 dark:bg-stone-900 dark:text-white p-3 rounded-full"
+    ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+        />
+    </svg></button
+>
+
+<footer
+    class="dark:bg-gray-700 absolute bottom-0 w-full text-center text-white"
+>
     <p class="text-xs">Buit with love by</p>
     <strong
         ><a
